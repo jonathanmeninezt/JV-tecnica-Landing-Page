5be52cf309dfa2115a607a61d962464ad53be644
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-01-09 13:55:58 -0300

    Ediação - modo responsivo mobile

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Manutenção Lava e Seca">
+    <meta name="keywords" content="Jv técnica,lava e seca,manutenção,máquina de lavar">
+    <meta name="author" content="Jonathan Meninez">
+    <title>JV Técnica</title>
+    <script src="https://kit.fontawesome.com/0537ae62fc.js" crossorigin="anonymous"></script>
+    <link href="<?php bloginfo('template_directory'); ?>/style.css" rel="stylesheet" > 
+    
+ </head>
+ <body>
+    <section class="container">
+        <header>
+
+                <div class="top-menu">
+                    <img src="<?php bloginfo('template_url'); ?>/img/logo.webp">
+                <nav class="menu-desktop">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Informações</a></li>
+                        <li><a href="">Serviços</a></li>
+                        <li><a href="">Contatos</a></li>
+                    </ul>
+                </nav><!--desktop-->
+
+                <nav class="menu-mobile">
+                    <i class="fa fa-bars"></i>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Informações</a></li>
+                        <li><a href="">Serviços</a></li>
+                        <li><a href="">Contatos</a></li>
+                    </ul>
+                </nav><!--menu-mobile-->
+            </div><!--top-menu-->
+            
+        </header>
+    </section><!--container-->
+
+
+    <section class="container" style="background-image: url('<?php bloginfo('template_url'); ?>/img/manute.webp');background-size: 100% 100%; background-repeat: no-repeat;background-position: center; width: 100%;height: 740px;">
+        <div class="center">
+            <div class="box-text">
+                <div class="box1"></div>
+                <h2>A sua Lava e Seca quebrou?</h2>
+                <span> Podemos te ajudar! Somos da JV Técnica, especializados em Lava e Seca da marca LG e Samsung.</span>
+                <div class="box2"></div>
+            </div><!--box-text-->
+            <input onclick="window.location.href='https:api.whatsapp.com/send?phone=5511992223789';"  class="botao" type="submit" value="Envie uma Mensagem"/>
+            <a href="https://wa.me/5511992223789" target="_blank" >
+            <button type="submit"><i class="fa-brands fa-whatsapp"></i></button>
+            </a>
+        </div><!--center-->
+    </section><!--container-->
+
+    <section class="container" style="background-color: rgb(30, 30, 116); height: 480px;">
+        <div class="center-box">
+            <div class="service-img">
+                <img src="<?php bloginfo('template_url'); ?>/img/sg.webp">
+                <img class="img-1" src="<?php bloginfo('template_url'); ?>/img/sgsimbol.webp">
+            </div><!--service-img-->
+            <div class="service-text">
+                <h2>Conserte ou faça manutenção da sua Lava e Seca, sem sair de casa!</h2>
+                <p>Atendemos em todas as regiões do estado de SãoPaulo, nossos técnicos vão até 
+                você,<br> fazendo todo o serviço sem a necessidade de sair da sua casa.</p>
+                <h4>* De acordo com a região, poderão ser cobradas taxas de visita.</h4>
+            </div><!--service-text-->
+        </div><!--center-box-->
+    </section><!--container-->
+
+    <section class="container2" style="background-color:rgb(30, 30, 116); height: 480px; width: 100%; margin-top: 500px;">
+        <div class="center-box2">
+            <div class="service-img2">
+                <img src="<?php bloginfo('template_url'); ?>/img/lg.webp">
+                <img class="img-2" src="<?php bloginfo('template_url'); ?>/img/lgsimbol.webp">
+            </div><!--service-img2-->
+            <div class="service-text2">
+                <h2>Assistência técnica especializada em Máquinas Lava e Seca LG!</h2>
+                <p>Com uma equipe de ampla experiência, realizamos serviços de instalação, conserto e manutenção<br> 
+                    de máquinas lava e seca, das principais marcas e modelos do mercado.<br> 
+                 </p>
+                
+            </div><!--service-text2-->
+        </div><!--center-box2-->
+    </section><!--container2-->
+
+    <section class="container-service">
+     <div class="center">
+        <div class="titulo-service">
+            <h2>Nossos Serviços</h2>
+        </div><!--titulo-service-->
+        <div class="big-box">
+            <div class="box1"><i class="fa-sharp fa-solid fa-gear"></i><h2>Conserto</h2></div><!--box1-->
+            <div class="box2"><i class="fa-solid fa-screwdriver-wrench"></i><h2>Instalação</h2></div><!--box2-->
+            <div class="box3"><i class="fa-solid fa-briefcase-medical"></i><h2>Manutenção Preventiva e Corretiva</h2></div><!--box3-->
+            <div class="box4"><i class="fa-solid fa-star"></i><h2>Higienização</h2></div><!--box4-->
+        </div><!--big-box-->
+     </div><!--center-->
+    </section><!--container-service-->
+
+    
+    <Section class="imagem-jv" style="background: url('<?php bloginfo('template_url'); ?>/img/JVtecnica.webp')center fixed;background-size: 70% 100%;background-repeat: no-repeat;width: 100%;height: 700px;">
+      <div class="center"></div><!--center-->
+    </Section><!--imagem-jv-->
+
+
+    <footer>
+        <div class="center">
+            <h2>Contatos</h2>
+            <img src="<?php bloginfo('template_url'); ?>/img/logo.webp">
+                <div class="end-page">
+                    <span><i class="fa-solid fa-location-dot"></i>Rua Júlio Paskevics, 46- Parque Santa Teresa, Carapicuíba - SP</span>
+                    <span><i class="fa-solid fa-envelope"></i>jvtecnicassitencia@gmail.com</span>
+                    <span onclick="window.location.href='tel:5511992223789';"><i class="fa-solid fa-phone"></i>(11)4303-3840</span>
+                    <a href="https://wa.me/5511992223789" target="_blank">
+                    <span ><i class="fa-brands fa-whatsapp"></i>(11)9 9222-3789</span>
+                    </a>
+                </div><!--end-page-->
+                <div class="icons">
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                </div><!--icons-->    
+        </div><!--center-->
+    </footer>
+    
+
+    <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+ </body>
+</html>
